1a3734bb6d1b05ef83b688ebc49f2a19a68ab1e5
Victor Elias <victorgelias@gmail.com>
2016-12-02 16:25:33 -0200

    Use more resilient way of getting hostname
    If app is under a proxy which is trusted, the hostname property
    will be set to the X-Forwarded-Host header instead, so it's more
    appropriate to be used.
    
    Only using it if available, since on parse.com the hostname
    property is undefined.
    
    Also using express's get instead of Connect's headers dict.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+function hostname(req) {
+  if (req.hostname) {
+    return req.hostname;
+  }
+  return req.get('Host');
+}
+
 module.exports = function () {
   return function parseExpressHttpsRedirect(req, res, next) {
     if (!req.secure) {
-      res.redirect('https://' + req.headers.host + req.originalUrl);
+      res.redirect('https://' + hostname(req) + req.originalUrl);
     } else {
       next();
     }
